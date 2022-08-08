8bbe266a73afd09c2463799ae484e7177ed80a65
cherrynoize <cherrynoize9987@outlook.com>
2022-08-08 22:52:27 +0200

    fixed br 3

--- public/sprinkle.js
+++ public/sprinkle.js
@@ -1,23 +1,31 @@
 window.onload = sprinkle;
 
+function replace(text, s, ss)
+    const words = text.split(s);
+    var value = "";
+
+    for (const w of words) {
+        const chars = w.split("");
+        for (const c of chars) {
+            if (c == " ") {
+                value += c;
+            } else if (c != "") {
+                value += "<span>" + c + "</span>";
+            }
+        }
+
+        value += "</h1>" + ss + "<h1 class='colors'>";
+    }
+
+    return value;
+}
+
 function sprinkle() {
     const colors = document.querySelectorAll(".colors");
     [...colors].forEach(function (p) {
-        const text = p.innerHTML;
-        const words = text.split("||");
-        p.innerHTML = "";
-
-        for (const w of words) {
-            const chars = w.split("");
-            for (const c of chars) {
-                if (c == " ") {
-                    p.innerHTML += c;
-                } else if (c != "") {
-                    p.innerHTML += "<span>" + c + "</span>";
-                }
-            }
-
-            p.innerHTML += "</h1><wbr><h1 class='colors'>";
-        }
+        var text = p.innerHTML;
+        text = replace(text, "|||", "<br>");
+        text = replace(text, "||", "<wbr>");
+        p.innerHTML = text;
     });
 }
