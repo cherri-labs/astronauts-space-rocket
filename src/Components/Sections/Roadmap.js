8ad17a20149b8c1d34faa9a32b416029caf16107
cherrynoize <cherrynoize9987@outlook.com>
2022-09-20 13:29:40 +0200

    border-blink

--- src/Components/Sections/Roadmap.js
+++ src/Components/Sections/Roadmap.js
@@ -13,10 +13,10 @@ export default function Roadmap() {
     <div className="card">
     <h3>A New World</h3>
     <p>
-    At the horizon dense degraded circuitry under gradient
-    twilight lights glimpse into a world where shadow
-    obstructs and clutters with uneven machinery the
-    metropolises' skylines.
+    At the <strong>Horizon Centre</strong> dense degraded
+    circuitry under the gradient twilight stars glimpsed into a
+    world where shadow obstructs and clutters with uneven
+    machinery the metropolises' skyline.
                                                   </p>
     <p>
     They call them <strong>Mechas</strong>, and they're
@@ -24,12 +24,13 @@ export default function Roadmap() {
                                                  </p>
     <p>
     But through the <strong>Rebels</strong>' uncorrupted eye we see
-    mechanisms rotten to the core rule on every street, school,
-    park, nightclub, rest home, motel room and gambling den in town.
-                                                    </p>
+    mechanisms rotten to the core that rule on every street,
+    merry-go-round, brothel, strip show, motel room and gambling den
+    in town.
+           </p>
     <p>
     As the night lurks in the <strong>Underground</strong> awakes,
-    and a thousand windows, cars, motherboards, air conditioners
+    and a thousand windows, <strong>Phantoms</strong>, motherboards
     and neon signs light up all at the same time.
                                             </p>
     <p>
@@ -43,27 +44,25 @@ export default function Roadmap() {
     <div className="slide">
     <div className="card-wrap">
     <div className="card">
-    <h3>Long and Winding Road</h3>
+    <h3>A Digital Realm</h3>
     <p>
-    That's what we plan to go through, with all of you.
-                                                  </p>
-    <p>
-    And through all the universes that are about to
-    spawn—through the generation that will state once and for
-    all what identity is and how we can define it—we are going
-    to stick through whatever turning point in the long and
-    winding road ahead.
+    In this evolving digital realm, new and varied worlds
+    spawn on everyday basis. With so many people coming and going
+    it's hard to keep a steady eye on things.  That's why this project
+    is so important to us. <b>We will
+    not let our values drift and we will keep our goals on top
+    of priorities at all times.</b>
     </p>
     <p>
-    And as messy as that is, we will try our best to get the
-    way down clear and linear for you to travel upon.
-                                     </p>
+    Because we all need something
+    to believe in.
+    </p>
     <p>
-    That's why we believe in <b>minimalism</b>. Whenever we provide
-    you with information, software, interfaces, we try to keep
-    it as simple and stupid as possible—so the important stuff
-    is always in clear sight.
-</p>
+    The next generation is coming and it will state once and for
+    all what identity is and how it is defined. And as messy as
+    that is, we will try our best to get the path down in a clear
+    and linear way for you to travel upon.
+                                     </p>
     </div>
     </div>
     </div>
@@ -72,80 +71,141 @@ export default function Roadmap() {
     <div className="slide">
     <div className="card-wrap">
     <div className="card">
-    <h3>The End of an Era</h3>
+    <h3>The Way Things Were</h3>
     <p>
-    Our world used to be one of a kind. A distinctive mark
+    Our world used to be <b>one of a kind</b>. A distinctive mark
     in an otherwise unholy chaos of wishful thinking and
-    abstract themes. Now, we have to adapt.
-</p>
+    abstract reality. Now, we have to adapt.
+    </p>
+    </div>
+    </div>
+    </div>
+    </article>
+    <article>
+    <div className="slide">
+    <div className="card-wrap">
+    <div className="card">
+    <h3>The Future</h3>
     <p>
-    We have to take what we can and discriminate the rest.
-                                          </p>
-    <p>
-    Our choices <i>pave</i> the way before us.
-                                         </p>
-    <p>
-    Change doesn't happen overnight, but one <b>giant step</b>
-    each day.
-            </p>
-    <p>
-    The line that separates us from our dreams becomes thinner
-    at exponentially growing paces.
+    The line that separates us from our dreams <b>becomes thinner
+    and thinner.</b>
     </p>
     <p>
-    It's the <b>end of an era</b>, but
-    the beginning of a different one.
-                                </p>
+    We have to take what we can and let go of the rest.
+                                          </p>
+    <p>
+    <b>Our choices <i>pave</i> the way before us.</b>
+    </p>
     </div>
     </div>
     </div>
     </article>
     <article>
     <div className="slide">
+    <div className="card-wrap">
     <div className="card">
-    <h3>Q2 &#183;&nbsp; Landing</h3>
-    <ul>
-    <li><strong>Space Buds</strong> public mint</li>
-    <li>We release <strong>Space Commanders</strong> to the world</li>
-    <li>Discord server</li>
-    <li>Lock all funds from the mint in the <strong>Astro Bank</strong></li>
-    </ul>
+    <h3>Changes</h3>
+    <p>
+    Change doesn't happen overnight, but <b>one tiny
+    step at a time.</b>
+    </p>
     </div>
-    </div>
-    </article>
-    <article>
-    <div className="slide">
-    <div className="card locked">
-    <h3>Q3 &#183;&nbsp; Terraforming</h3>
-    <ul>
-    <li>Full d0x goes public</li>
-    <li><strong>Space Rocket</strong></li>
-    <li>We let go of <strong>Lonely Astronauts</strong></li>
-    <li>Staking portal implementation for <strong>LASR</strong></li>
-    </ul>
     </div>
     </div>
     </article>
     <article>
     <div className="slide">
-    <div className="card locked">
-    <h3>Q4 &#183;&nbsp; Expansion</h3>
-    <ul>
-    <li><strong>Detached Aliens</strong> and <strong>Unhappy Clones</strong> join the club</li>
-    <li>Official puzzle competition for owners</li>
-    </ul>
+    <div className="card-wrap">
+    <div className="card">
+    <h3>Minimalism</h3>
+    <p>
+    Whenever we provide
+    you with information, software, interfaces, <b>we try to keep
+    it as simple and stupid as possible—so the important stuff
+    is always in clear sight.</b>
+    </p>
+    </div>
     </div>
     </div>
     </article>
     <article>
     <div className="slide">
+    <div className="card-wrap">
+    <div className="card">
+    <h3>Landing</h3>
+    <p>
+    As we approach <b>landing phase</b> we are going to release{" "}
+    <strong>Space Buds</strong> to the world. After <i>whitelist
+    phase</i> and <i>public mint</i>, <b>32</b> <strong>Space
+    Commanders</strong> will be airdropped to holders and giveaway
+    winners, together with the remaining portion of <strong>Space
+    Buds</strong> NFTs.
+    </p>
+    <p>
+    All funds from the mint will be immediately locked into
+    the <strong>Astro Bank</strong> where users will be
+    able to claim their reward right away.
+               </p>
+    </div>
+    </div>
+    </div>
+    </article>
+    <article>
+    <div className="slide">
+    <div className="card-wrap">
     <div className="card locked">
-    <h3>Q5 &#183;&nbsp; Quest to Faraway</h3>
-    <ul>
-    <li>First glimpse into the <strong>LASR</strong> metaverse</li>
-    <li><strong>$STRD</strong> token is airdropped to holders</li>
-    <li>Instantiate voting mechanism for community decisions</li>
-    </ul>
+    <h3>Terraforming</h3>
+    <p>
+    We work everyday to extend the current
+    documentation about the project, which we plan to
+    release during <b>Terraforming</b>.
+                                  </p>
+    <p>
+    We will also be opening a <strong>Space Club</strong> for
+    owners, where they can hop in for a chat, drink or
+    just to buy stuff.
+                 </p>
+    <p>
+    After <strong>Lonely Astronauts</strong> mint we implement
+    the staking portal and <strong>$STRD</strong> tokens are
+    airdropped to owners only.
+    </p>
+    <p>
+    </p>
+    </div>
+    </div>
+    </div>
+    </article>
+    <article>
+    <div className="slide">
+    <div className="card-wrap">
+    <div className="card locked">
+    <h3>Expansion</h3>
+    <p>
+    We release <strong>Mechas</strong> and <strong>Rebels</strong> to
+    the world.
+    </p>
+    <p>
+    The <strong>LASR</strong> DAO is created.
+                                        </p>
+    </div>
+    </div>
+    </div>
+    </article>
+    <article>
+    <div className="slide">
+    <div className="card-wrap">
+    <div className="card locked">
+    <h3>Quest to Faraway</h3>
+    <p>
+    All owners can participate in the <strong>Grand Race for
+    Space</strong> and take their chance to win the big
+    money prize.
+               </p>
+    <p>
+    The <strong>LASR</strong> metaverse finally arrives.
+    </p>
+    </div>
     </div>
     </div>
     </article>
